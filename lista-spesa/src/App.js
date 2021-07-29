51f63e6effb08bd53eb1ba995c7314968ff42501
Davide Cardia <davide.cardia@spindox.it>
2021-07-29 23:09:47 +0200

    redirect to homepage

--- lista-spesa/src/App.js
+++ lista-spesa/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Carrello from "./pages/Carrello";
 import Product from "./pages/Product";
@@ -9,7 +9,9 @@ import Login from "./pages/Login";
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={Login}>
+        {/* TODO: REDIRECT se logged true */} <Redirect to="/homepage" />
+      </Route>
       <Route exact path="/homepage" component={Homepage} />
       <Route exact path="/product/:id" component={Product} />
       <Route exact path="/carrello" component={Carrello} />
